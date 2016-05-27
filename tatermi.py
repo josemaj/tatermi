e1cfb71b65167b355e034a86b6e98b297f1c2d98
josemaj <josemariabellber@gmail.com>
2016-05-27 13:19:09 +0200

    Update tatermi.py

--- tatermi.py
+++ tatermi.py
@@ -1,7 +1,8 @@
 # programmed in python 
 # by  josemaj 
 # published 27-5-2016 
-
+#everything is under  GNU General Public License
+#http://www.gnu.org/licenses/gpl-3.0.html
 
 life = 100
 money = 50 
